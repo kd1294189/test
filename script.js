3bfb4b1953dfaedad9c3840986cd2306e512c7cf
GitHubkd1294189 <kd1294189@st.kobedenshi.ac.jp>
2024-02-08 14:14:33 +0900

    a

--- script.js
+++ script.js
@@ -75,21 +75,24 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     function renderHandButtons() {
-        /*
+        
         handContainer.innerHTML = '';
         for (let i = 0; i < drawnCards.length; i++) {
 
             const cardButton = document.createElement('button');
             cardButton.classList.add('hand-button');
             cardButton.textContent = drawnCards[i];
+
+            /*
             cardButton.addEventListener('click', function() {
                 // ボタンを削除し、カードを札山に戻す
                 cards.push(drawnCards[i]);
                 drawnCards.splice(i, 1);
                 renderHandButtons();
             });
+            */
             handContainer.appendChild(cardButton);
         }
-        */
+        
     }
 });
