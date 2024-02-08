9fa13e86645a25dd1cc3391eb55e169c6c3b80bb
GitHubkd1294189 <kd1294189@st.kobedenshi.ac.jp>
2024-02-08 13:17:27 +0900

    本格的に開始

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const drawnCardsDisplay = document.getElementById('drawn-cards');
     const handContainer = document.getElementById('hand');
     
-    let cards = ['エース', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'ジャック', 'クイーン', 'キング'];
+    //let cards = ['エース', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'ジャック', 'クイーン', 'キング'];
+
+    let cards = ['リンゴ×1','リンゴ×2','リンゴ×3','リンゴ×4','リンゴ×5','リンゴ×6', 
+    'ニンジン×1', 'ニンジン×2', 'ニンジン×3', 'ニンジン×4', 'ニンジン×5', 'ニンジン×6', 
+    'ジャガイモ×1',  'ジャガイモ×2', 'ジャガイモ×3', 'ジャガイモ×4', 'ジャガイモ×5', 'ジャガイモ×6',
+    'レタス×1',  'レタス×2', 'レタス×3', 'レタス×4', 'レタス×5', 'レタス×6',
+    'トマト×1', 'トマト×2', 'トマト×3', 'トマト×4', 'トマト×5', 'トマト×6',
+    'ピーマン×1', 'ピーマン×2', 'ピーマン×3', 'ピーマン×4', 'ピーマン×5', 'ピーマン×6'];
+
     let drawnCards = [];
     
     drawCardButton.addEventListener('click', function() {
@@ -25,6 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function renderHandButtons() {
         handContainer.innerHTML = '';
         for (let i = 0; i < drawnCards.length; i++) {
+
             const cardButton = document.createElement('button');
             cardButton.classList.add('hand-button');
             cardButton.textContent = drawnCards[i];
