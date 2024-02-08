a73a3a7970c484dff3a13447ccce39fa50189f44
GitHubkd1294189 <kd1294189@st.kobedenshi.ac.jp>
2024-02-08 12:43:24 +0900

    改良

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
             cards.splice(randomIndex, 1); // デッキから引いたカードを削除
             drawnCards.push(randomCard); // 引かれたカードを drawnCards 配列に追加
             cardDisplay.textContent = '引いたカード: ' + randomCard;
-            drawnCardsDisplay.textContent = '引かれたカード: ' + drawnCards.join(', ');
+            //drawnCardsDisplay.textContent = '引かれたカード: ' + drawnCards.join(', ');
             renderHandButtons();
         }
     });
